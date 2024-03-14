51d4f3a2403eadf009ce105f56c7c69bbe8a81a1
MichaelGift <simiyugift@outlook.com>
2024-03-14 20:20:12 +0300

    Feat: Provide application context via hilt

---
+++ app/src/main/java/com/myth/ticketmasterapp/common/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.myth.ticketmasterapp.common.di
+
+import android.app.Application
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideAppContext(app: Application): Context {
+        return app.applicationContext
+    }
+}
