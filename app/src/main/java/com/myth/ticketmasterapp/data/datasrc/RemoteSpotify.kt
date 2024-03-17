f5608273b4c36f350c3d29c21613a96f856595d6
MichaelGift <simiyugift@outlook.com>
2024-03-17 22:22:52 +0300

    Refactor: Rename SpotifyRemoteDataSource to RemoteSpotify

--- app/src/main/java/com/myth/ticketmasterapp/data/datasrc/RemoteSpotify.kt
+++ app/src/main/java/com/myth/ticketmasterapp/data/datasrc/RemoteSpotify.kt
@@ -1,11 +1,11 @@
-package com.myth.ticketmasterapp.data.datasource
+package com.myth.ticketmasterapp.data.datasrc
 
 import com.myth.ticketmasterapp.data.spotifydatamodels.AccessTokenResponse
 import com.myth.ticketmasterapp.data.spotifydatamodels.SpotifyData
 import retrofit2.Call
 import retrofit2.Response
 
-interface SpotifyRemoteDataSource {
+interface RemoteSpotify {
     suspend fun getSpotifyData(authorization: String, artist: String): Response<SpotifyData>
     fun getAccessToken(authorization: String, grantType: String): Call<AccessTokenResponse>
 }
