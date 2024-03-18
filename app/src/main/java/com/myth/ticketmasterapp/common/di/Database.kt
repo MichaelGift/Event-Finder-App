b86a5ba4d47ef38b08fdb404678b48732eb433c0
MichaelGift <simiyugift@outlook.com>
2024-03-18 23:44:02 +0300

    Feat: Provide database

---
+++ app/src/main/java/com/myth/ticketmasterapp/common/di/Database.kt
@@ -0,0 +1,31 @@
+package com.myth.ticketmasterapp.common.di
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.myth.ticketmasterapp.data.db.EventDao
+import com.myth.ticketmasterapp.data.db.EventDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Database {
+
+    @Singleton
+    @Provides
+    fun providesLocalDatabase(context: Context): RoomDatabase {
+        return Room.databaseBuilder(
+            context, EventDatabase::class.java, DB_NAME
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesEventDao(eventDatabase: EventDatabase): EventDao {
+        return eventDatabase.eventDao()
+    }
+}
