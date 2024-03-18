1bc4cfa544afaafede7b136beaddbd36d5daf353
MichaelGift <simiyugift@outlook.com>
2024-03-18 23:44:36 +0300

    Feat: Constructor inject event repository

--- app/src/main/java/com/myth/ticketmasterapp/domain/usecases/DeleteEventUseCase.kt
+++ app/src/main/java/com/myth/ticketmasterapp/domain/usecases/DeleteEventUseCase.kt
@@ -1,8 +1,12 @@
 package com.myth.ticketmasterapp.domain.usecases
 
+import com.myth.ticketmasterapp.data.EventRepositoryImplementation
 import com.myth.ticketmasterapp.data.eventdatamodels.Event
 import com.myth.ticketmasterapp.domain.repository.EventRepository
+import javax.inject.Inject
 
-class DeleteEventUseCase(private var eventRepository: EventRepository){
+class DeleteEventUseCase @Inject constructor(
+    private var eventRepository: EventRepository
+){
     suspend fun execute(event: Event) = eventRepository.deleteEvent(event)
 }
--- app/src/main/java/com/myth/ticketmasterapp/domain/usecases/SaveEventUseCase.kt
+++ app/src/main/java/com/myth/ticketmasterapp/domain/usecases/SaveEventUseCase.kt
@@ -1,8 +1,12 @@
 package com.myth.ticketmasterapp.domain.usecases
 
+import com.myth.ticketmasterapp.data.EventRepositoryImplementation
 import com.myth.ticketmasterapp.data.eventdatamodels.Event
 import com.myth.ticketmasterapp.domain.repository.EventRepository
+import javax.inject.Inject
 
-class SaveEventUseCase(private var eventRepository: EventRepository) {
+class SaveEventUseCase @Inject constructor(
+    private var eventRepository: EventRepository
+) {
     suspend fun execute(event: Event) = eventRepository.saveEvent(event)
 }
