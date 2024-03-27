d2956c6421abd6197c9d94381b852c8d98989ca4
Michael Gift <simiyugift@outlook.com>
2024-03-27 20:58:21 +0300

    Remove redundant qualifier

--- app/src/main/java/com/myth/ticketmasterapp/data/eventdatamodels/Embedded.kt
+++ app/src/main/java/com/myth/ticketmasterapp/data/eventdatamodels/Embedded.kt
@@ -1,5 +1,5 @@
 package com.myth.ticketmasterapp.data.eventdatamodels
 
 data class Embedded(
-    val events: List<com.myth.ticketmasterapp.data.eventdatamodels.Event>
+    val events: List<Event>
 )
