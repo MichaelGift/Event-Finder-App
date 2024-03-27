971d4440ecad115c9ba0e630fd98c8ccfb049ad6
Michael Gift <simiyugift@outlook.com>
2024-03-27 21:19:34 +0300

    remove unneeded single quotes

--- app/src/main/java/com/myth/ticketmasterapp/data/eventmodels/Sales.kt
+++ app/src/main/java/com/myth/ticketmasterapp/data/eventmodels/Sales.kt
@@ -6,5 +6,5 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Sales(
     val presales: List<com.myth.ticketmasterapp.data.eventmodels.Presale>,
-    val `public`: com.myth.ticketmasterapp.data.eventmodels.Public
+    val public: com.myth.ticketmasterapp.data.eventmodels.Public
 ):Parcelable
