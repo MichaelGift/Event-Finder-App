0f68992d4fbdc3ca504a4a8fe6d5fdd4a824cf4c
Michael Gift <simiyugift@outlook.com>
2024-03-28 21:34:32 +0300

    Add caching logic

--- app/src/main/java/com/myth/ticketmasterapp/common/di/Retrofit.kt
+++ app/src/main/java/com/myth/ticketmasterapp/common/di/Retrofit.kt
@@ -1,5 +1,6 @@
 package com.myth.ticketmasterapp.common.di
 
+import android.content.Context
 import com.myth.ticketmasterapp.BuildConfig
 import com.myth.ticketmasterapp.data.api.SpotifyAuthService
 import com.myth.ticketmasterapp.data.api.SpotifyService
@@ -8,6 +9,9 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.Retrofit
 import javax.inject.Named
@@ -16,6 +20,30 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object Retrofit {
+
+    @Provides
+
+    fun provideOkHTTPClientCache(context: Context): OkHttpClient {
+        val cache = Cache(context.cacheDir, CACHE_SIZE)
+        val interceptor = Interceptor { chain ->
+            val originalResponse = chain.proceed(chain.request())
+            val cacheControl = originalResponse.header("Cache-Control")
+            if (cacheControl == null || cacheControl.contains("no-store") || cacheControl.contains("no-cache") ||
+                cacheControl.contains("must-revalidate") || cacheControl.contains("max-age=0")
+            ) {
+                originalResponse
+            } else {
+                val maxAge = 60
+                originalResponse.newBuilder().header("Cache-Control", "public, max-age=$maxAge")
+                    .build()
+            }
+        }
+        return OkHttpClient.Builder()
+            .cache(cache)
+            .addInterceptor(interceptor)
+            .build()
+    }
+
     @Singleton
     @Provides
     @Named("ticketMaster")
@@ -29,8 +57,11 @@ object Retrofit {
     @Singleton
     @Provides
     @Named("spotify")
-    fun providesSpotifyRetrofit(): Retrofit {
+    fun providesSpotifyRetrofit(
+        cacheClient: OkHttpClient
+    ): Retrofit {
         return Retrofit.Builder()
+            .client(cacheClient)
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(BuildConfig.SPOTIFY_BASE_URL)
             .build()
--- app/src/main/java/com/myth/ticketmasterapp/data/api/TicketMasterService.kt
+++ app/src/main/java/com/myth/ticketmasterapp/data/api/TicketMasterService.kt
@@ -3,10 +3,12 @@ package com.myth.ticketmasterapp.data.api
 import com.myth.ticketmasterapp.data.eventmodels.EventsData
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 
 interface TicketMasterService {
+    @Headers("Cache-Control: max-age=3600")
     @GET("events")
     suspend fun getEventByName(
         @Query("apikey") apikey: String,
