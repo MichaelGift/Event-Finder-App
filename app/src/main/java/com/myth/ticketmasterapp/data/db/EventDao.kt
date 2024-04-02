2dfaef674f10ce4b6e185be2c7835f729cded143
Michael Gift <simiyugift@outlook.com>
2024-04-02 23:05:47 +0300

    Remove ROOM impl

--- app/src/main/java/com/myth/ticketmasterapp/data/db/EventDao.kt
+++ app/src/main/java/com/myth/ticketmasterapp/data/db/EventDao.kt
@@ -3,18 +3,13 @@ package com.myth.ticketmasterapp.data.db
 import androidx.room.*
 import com.myth.ticketmasterapp.data.eventmodels.Event
 
-@Dao
 interface EventDao {
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveEvents(event: Event)
 
-    @Delete
     suspend fun deleteEvent(event: Event)
 
-    @Query("SELECT * FROM events")
     suspend fun getEvents(): List<Event>
 
-    @Query("SELECT * FROM events  WHERE id = :eventId")
     fun getEventById(eventId : String): List<Event>?
 }
--- app/src/main/java/com/myth/ticketmasterapp/data/db/EventDatabase.kt
+++ app/src/main/java/com/myth/ticketmasterapp/data/db/EventDatabase.kt
@@ -5,21 +5,21 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.myth.ticketmasterapp.data.typeconverters.*
 
-@Database(
-    entities = [com.myth.ticketmasterapp.data.eventmodels.Event::class],
-    version = 1,
-    exportSchema = false
-)
-@TypeConverters(
-    ClassificationXListConverter::class,
-    DatesConverter::class,
-    EmbeddedXConverter::class,
-    ImageXXConverter::class,
-    PriceRangeConverter::class,
-    PromoterConverter::class,
-    SalesConveter::class,
-    SeatMapConverter::class,
-)
+//@Database(
+//    entities = [com.myth.ticketmasterapp.data.eventmodels.Event::class],
+//    version = 1,
+//    exportSchema = false
+//)
+//@TypeConverters(
+//    ClassificationXListConverter::class,
+//    DatesConverter::class,
+//    EmbeddedXConverter::class,
+//    ImageXXConverter::class,
+//    PriceRangeConverter::class,
+//    PromoterConverter::class,
+//    SalesConveter::class,
+//    SeatMapConverter::class,
+//)
 abstract class EventDatabase : RoomDatabase() {
     abstract fun eventDao(): EventDao
 }
